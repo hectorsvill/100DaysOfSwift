4c742652bc3223d0af5c601d8d9139648823fc4d
Hector Steven <hectorsteven@Hectors-MBP.attlocal.net>
2019-02-19 23:15:16 -0800

    using file system to get pictue

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Hector Steven on 2/19/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        let fm = FileManager.default //data type that lets us work with the filesystem
+        let path = Bundle.main.resourcePath! //directory path to pictures
+        let items = try! fm.contentsOfDirectory(atPath: path)   //The items constant will be an array of strings containing filenames.
+        
+        for item in items {
+            //set pictures
+            if item.hasPrefix("nssl") {
+                //
+                print(item.description)
+            }
+        }
+    }
+
+
+}
+
