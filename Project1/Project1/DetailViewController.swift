d1c66e41be6ba3dcb851539936a3cbb112c8082d
Hector Steven <hectorsteven@Hectors-MBP.attlocal.net>
2019-02-20 12:32:47 -0800

    building a detail screen

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Hector Steven on 2/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var ImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
