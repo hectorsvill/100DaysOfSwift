99c45d617dcafa678621e5d110871e36a9c88b43
hvillasa <hvillasano@go.pasadena.edu>
2019-03-01 16:41:36 -0800

    project 5

--- MyPlay.playground/Contents.swift
+++ MyPlay.playground/Contents.swift
@@ -1,25 +1,35 @@
 import UIKit
 
 
-var numlist: [Int] = [5, 2, -17, -8, 4, 10, -9, 3]
+//var numlist: [Int] = [5, 2, -17, -8, 4, 10, -9, 3]
+//
+//
+//func findHigh(nums: [Int]) -> Int?
+//{
+//	var high: Int?
+//
+//	high = numlist[0]
+//	for n in nums
+//	{
+//		if n > high!
+//		{
+//			high = n
+//		}
+//	}
+//
+//	return high
+//}
+//
+//findHigh(nums: numlist)
+//var nums = numlist.sorted()
+//print(nums[nums.count - 1])
 
+var word = "rrun"
+var ogword = "running"
 
-func findHigh(nums: [Int]) -> Int?
-{
-	var high: Int?
-	
-	high = numlist[0]
-	for n in nums
-	{
-		if n > high!
-		{
-			high = n
-		}
+for l in word {
+	if let p = ogword.firstIndex(of: l){
+		print(ogword)
+		ogword.remove(at: p)
 	}
-
-	return high
 }
-
-findHigh(nums: numlist)
-var nums = numlist.sorted()
-print(nums[nums.count - 1])
--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -43,8 +43,8 @@ class ViewController: UITableViewController {
 		tableView.reloadData()
 	}
 	
-	
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	override func tableView(_ tableView: UITableView,
+							numberOfRowsInSection section: Int) -> Int {
 		return usedWords.count
 	}
 
@@ -85,25 +85,59 @@ class ViewController: UITableViewController {
 		if isPossible(word: lowerAnswer) {
 			if isOriginal(word: lowerAnswer) {
 				if isReal(word: lowerAnswer) {
-					usedWords.insert(answer, at: 0)
 
+					usedWords.insert(answer, at: 0)
 					let indexPath = IndexPath(row: 0, section: 0)
 					tableView.insertRows(at: [indexPath], with: .automatic)
+
+					return
+				} else {
+				notPossibleOriginalReal(errorTile: "Word not recognised", errorMessage: "Word is invalid!")
 				}
+			} else {
+			notPossibleOriginalReal(errorTile: "Word used already", errorMessage: "Only use a word ones!")
+				
 			}
+		}else {
+		notPossibleOriginalReal(errorTile: "Not Possible", errorMessage: "You cant spell that word from \(title!)")
+			
 		}
 	}
 
 	func isPossible(word: String) -> Bool {
+		guard var tempWord = title?.lowercased() else { return false}
+
+		for letter in word {
+			if let position = tempWord.firstIndex(of: letter) {
+				tempWord.remove(at: position)
+			} else {
+				return false
+			}
+		}
+
 		return true
 	}
 
 	func isOriginal(word: String) -> Bool {
-		return true
+		return !usedWords.contains(word)
 	}
 
 	func isReal(word: String) -> Bool {
-		return true
+		//check if word exist with uitectchecker
+		let checker = UITextChecker()
+		let range = NSRange(location: 0, length: word.utf16.count)
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range,
+															startingAt: 0,
+															wrap: false,
+															language: "en")
+
+		return (misspelledRange.location == NSNotFound) && (word.count > 3)
+	}
+	
+	func notPossibleOriginalReal(errorTile: String, errorMessage: String){
+		let ac = UIAlertController(title: errorTile, message: errorMessage, preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .default))
+		present(ac, animated: true)
 	}
 }
 
