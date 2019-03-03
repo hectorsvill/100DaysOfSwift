d917bf6d0895196fc3dc3dec5b9daa4c2f596627
hvillasa <hvillasano@go.pasadena.edu>
2019-03-03 12:43:25 -0800

    Project6a

--- MyPlaygroundForAlgorithims.playground/Contents.swift
+++ MyPlaygroundForAlgorithims.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 ////////////////////////////////////////////////////////////////////////////////
-//Stack - You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off.
+//Stack - You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off. LIFO
 
 public struct Stack<T> {
 	fileprivate var array = [T]()
@@ -30,13 +30,30 @@ public struct Stack<T> {
 }
 
 
-var stacks = Stack(array: [])
-stacks.isEmpty
-stacks.push("A")
-stacks.push("H")
-stacks.push("df")
-stacks.push("Q")
-stacks.push(2)
-print(stacks)
-stacks.pop()
-print(stacks)
+//var stacks = Stack(array: [])
+//stacks.isEmpty
+//stacks.push("A")
+//stacks.push("H")
+//stacks.push("df")
+//stacks.push("Q")
+//stacks.push(2)
+//print(stacks)
+//stacks.pop()
+//print(stacks)
+
+
+////////////////////////////////////////////////////////////////////////////////
+//Queue -A queue is a list where you can only insert new items at the back and remove items from the front. FIFO
+public struct Queue<T> {
+	fileprivate var array = [T] ()
+	
+	
+	public var isEmpty: Bool {
+		return array.isEmpty
+	}
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
