b9dcffc3c829e989e3f5c7b38e5fddc92255e409
hvillasa <hvillasano@go.pasadena.edu>
2019-03-04 11:35:35 -0800

    Milestone: Projects 4-6

---
+++ MilestoneProjects4-6/MilestoneProjects4-6/ShoppingList.swift
@@ -0,0 +1,27 @@
+//
+//  ShoppingList.swift
+//  MilestoneProjects4-6
+//
+//  Created by Hector Steven on 3/4/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingList: UITableViewController {
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+
+		
+		
+	}
+
+
+	
+	
+	
+	
+
+}
---
+++ MilestoneProjects4-6/MilestoneProjects4-6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MilestoneProjects4-6
+//
+//  Created by Hector Steven on 3/4/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
