7d7d906c8deca98ddd814195355bc0869dc70864
hvillasa <hvillasano@go.pasadena.edu>
2019-03-04 12:07:01 -0800

    Milestone: Projects 4-6

--- MilestoneProjects4-6/MilestoneProjects4-6/ShoppingList.swift
+++ MilestoneProjects4-6/MilestoneProjects4-6/ShoppingList.swift
@@ -12,18 +12,30 @@ import UIKit
 
 class ShoppingList: UITableViewController {
 
+	var shoppingList: [String] = []
+	
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
-
+		title = "Shopping List"
 		
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToShoppingList))
 		
 	}
 
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return shoppingList.count
+	}
 	
 	
-	
-	
+	@objc func addToShoppingList() {
+		shoppingList.append(" ")
+		
+		let ac = UIAlertController(title: "title", message: "Please add an Item", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "ok", style: .cancel))
+		present(ac, animated: true)
+	}
 
 }
