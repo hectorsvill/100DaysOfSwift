485c2f8881a6304545b45a1ab796d06dc411fa4a
hvillasa <hvillasano@go.pasadena.edu>
2019-03-04 22:27:58 -0800

    syntax

--- MilestoneProjects4-6/MilestoneProjects4-6/ShoppingList.swift
+++ MilestoneProjects4-6/MilestoneProjects4-6/ShoppingList.swift
@@ -10,11 +10,10 @@
 
 import UIKit
 
-class ShoppingList: UITableViewController {
-
+class ShoppingList: UITableViewController
+{
 	var shoppingList: [String] = []
 
-	
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
@@ -24,63 +23,60 @@ class ShoppingList: UITableViewController {
 			UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToShoppingList))
 		
 		navigationItem.leftBarButtonItem =
-			UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(removeItemsShare))
+			UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(removeItemsShare))
 	}
-//tableview/////////////////////////////////////////////////////////////////////
+
+	//tableview/////////////////////////////////////////////////////////////////
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
 	{
 		return shoppingList.count
 	}
-	
+
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
 	{
-		
 		let cell = tableView.dequeueReusableCell(withIdentifier: "listItem", for: indexPath)
 		
 		cell.textLabel?.text = shoppingList[indexPath.row]
 		return cell
 	}
 
-//@objc/////////////////////////////////////////////////////////////////////////
-	
+	//@objc/////////////////////////////////////////////////////////////////////
 	@objc func removeItemsShare()
 	{
-		
-		
 		let ac = UIAlertController(title: "Remove All / Share", message: nil, preferredStyle: .actionSheet)
-		ac.addAction(UIAlertAction(title: "Remove ALL", style: .default,handler: {
-			
+
+		ac.addAction(UIAlertAction(title: "Remove ALL", style: .default, handler:
+		{
 			(alert: UIAlertAction) -> Void in
 			self.shoppingList.removeAll()
 			self.tableView.reloadData()
 			
 		}))
-		
-		ac.addAction(UIAlertAction(title: "Share Shopping List", style: .default, handler: {
-			
+
+		ac.addAction(UIAlertAction(title: "Share Shopping List", style: .default, handler:
+		{
 			(alert: UIAlertAction) -> Void in
 			//put list in string
 			let sl = self.shoppingList.joined(separator: "\n")
-			
 			//share view
 			let vc = UIActivityViewController(activityItems: [sl], applicationActivities: [])
 			self.present(vc, animated: true)
 			
 		}))
+
 		present(ac, animated: true)
 	}
-	
-	
+
 	@objc func addToShoppingList()
 	{
 		let ac = UIAlertController(title: "Enter Item:", message: nil, preferredStyle: .alert)
-		
+
 		//add textfield
 		ac.addTextField()
-		//ac.addTextField()
-		
+
 		//grab text
-		let submitItem = UIAlertAction(title: "Submit", style: .default) {
+		let submitItem = UIAlertAction(title: "Submit", style: .default)
+		{
 			[weak self, weak ac] action in
 			guard let item = ac?.textFields?[0].text  else { return }
 			self?.addToTable(item)
@@ -89,19 +85,13 @@ class ShoppingList: UITableViewController {
 		ac.addAction(submitItem)
 		present(ac, animated: true)
 	}
-	
-	
-//func/////////////////////////////////////////////////////////////////////////
+
+	//func/////////////////////////////////////////////////////////////////////
 	func addToTable(_ item: String)
 	{
 		shoppingList.insert(item, at: 0)
-		//shoppingList.append(item)
-
 		let ip = IndexPath(row: 0, section: 0)
-
 		tableView.insertRows(at: [ip], with: .automatic)
-
 		print(shoppingList)
 	}
-
 }
