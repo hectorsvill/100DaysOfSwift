269dba957bb37055648399c3c496949471251ded
hvillasa <hvillasano@go.pasadena.edu>
2019-03-06 23:41:52 -0800

    algo

--- MyPlaygroundForAlgorithims.playground/Contents.swift
+++ MyPlaygroundForAlgorithims.playground/Contents.swift
@@ -29,31 +29,65 @@ public struct Stack<T> {
 	}
 }
 
-
-//var stacks = Stack(array: [])
-//stacks.isEmpty
-//stacks.push("A")
-//stacks.push("H")
-//stacks.push("df")
-//stacks.push("Q")
-//stacks.push(2)
-//print(stacks)
-//stacks.pop()
-//print(stacks)
-
-
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 //Queue -A queue is a list where you can only insert new items at the back and remove items from the front. FIFO
 public struct Queue<T> {
 	fileprivate var array = [T] ()
-	
-	
+
 	public var isEmpty: Bool {
 		return array.isEmpty
 	}
+	
+	public var count: Int {
+		return array.count
+	}
+	
+	public mutating func enqueue(_ element: T){
+		array.append(element)
+	}
+	
+	public  mutating func dequeue() -> T? {
+		if isEmpty{
+			return nil
+		} else {
+			return array.removeFirst()
+		}
+	}
+	
+	public var front: T? {
+		return array.first
+	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//Insertion Sort
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+var numarr = [Int]()
+
+for _ in 0...10 {
+	numarr.append(Int.random(in: 3...91))
+}
+print(numarr)
+
+func InsertionSort(_ arr: [Int]) -> [Int] {
+
+	var a = arr
+	for x in 1..<a.count {
+		var y = x
+		while a[y] < a[y - 1] && y > 0 {
+			a.swapAt(y - 1 , y)
+			y -= 1
+			print("\(y) \(a)")
+		}
+		
+	}
+	
+	return a
+}
+
+print(InsertionSort(numarr))
+
