cf8bb0149b38be73729d05b585673e1a0185d4b4
hvillasa <hvillasano@go.pasadena.edu>
2019-03-08 14:56:02 -0800

    scorelabel constraints

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var cluesLabel: UILabel!
+	var answerLabel: UILabel!
+	var currentAnswer: UILabel!
+	var scoreLabel: UILabel!
+	var letterButtons = [UIButton]()
+	
+	override func loadView() {
+		view = UIView()
+		view.backgroundColor = .white
+		
+		scoreLabel = UILabel()
+		
+		scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+		scoreLabel.textAlignment = .right
+		scoreLabel.text = "Score: 0"
+		view.addSubview(scoreLabel)
+		
+		NSLayoutConstraint.activate([
+			scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+			scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+			
+			//more constraints to be added
+			
+			
+			
+			])
+		
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
