5819f0a22a3be5c8ab2ef1f544e665e8126593be
hvillasa <hvillasano@go.pasadena.edu>
2019-03-12 22:54:29 -0700

    Project 1 8 9  multi threading

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -10,49 +10,50 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    var pictueres = [String]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = "Storm Viewer"
-        navigationController?.navigationBar.prefersLargeTitles = true
-    
-        let fm = FileManager.default //data type that lets us work with the filesystem
-        let path = Bundle.main.resourcePath! //directory path to pictures
-        let items = try! fm.contentsOfDirectory(atPath: path)   //The items constant will be an array of strings containing filenames.
-        
-        for item in items {
-            //set pictures
-            if item.hasPrefix("nssl") {
-                
-                pictueres.append(item)
-            }
-        }
-        pictueres = pictueres.sorted()
-        //for p in pictueres { print(p) }
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictueres.count
-    }
-    //cellfor -- set cell text
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        
-        //variable just holds a text -- pictures[indexPath.row]
-        cell.textLabel?.text = "\(indexPath.row + 1) of \(pictueres.count)" //set text in each table
-        
-        //print((cell.textLabel?.text)!)
-        return cell
-    }
-    //didselect - 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.selectedImage = pictueres[indexPath.row]
-            navigationController?.pushViewController(vc, animated: true)
-        }
-    }
+	var pictueres = [String]()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		title = "Storm Viewer"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		performSelector(inBackground: #selector(fetchImages), with: nil)
+		tableView.reloadData()
+	}
+	
+	@objc func fetchImages() {
+		let fm = FileManager.default //data type that lets us work with the filesystem
+		let path = Bundle.main.resourcePath! //directory path to pictures
+		let items = try! fm.contentsOfDirectory(atPath: path)   //The items constant will be an array of strings containing filenames.
+
+		for item in items {
+			//set pictures
+			if item.hasPrefix("nssl") {
+				pictueres.append(item)
+			}
+		}
+		pictueres = pictueres.sorted()
+		for p in pictueres { print(p) }
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return pictueres.count
+	}
+
+	//cellfor -- set cell text
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+		//variable just holds a text -- pictures[indexPath.row]
+		cell.textLabel?.text = "\(indexPath.row + 1) of \(pictueres.count)" //set text in each table
+		return cell
+	}
+
+	//didselect -
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+			vc.selectedImage = pictueres[indexPath.row]
+			navigationController?.pushViewController(vc, animated: true)
+		}
+	}
 }
 
--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -131,7 +131,8 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		loadLevel()
+		
+		performSelector(inBackground: #selector(loadLevel), with: nil)
 	}
 
 	@objc func letterTapped(_ sender: UIButton) {
@@ -176,7 +177,7 @@ class ViewController: UIViewController {
 		activatedButtons.removeAll()
 	}
 
-	func loadLevel() {
+	@objc func loadLevel() {
 		var clueString = ""
 		var solutionString = ""
 		var letterBits = [String]()
@@ -212,23 +213,28 @@ class ViewController: UIViewController {
 			}
 		}
 
-		//configure buttons anf labels
-		cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
-		answerLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
-		letterBits.shuffle()
-		if letterBits.count == letterButtons.count {
-			for i in 0 ..< letterButtons.count {
-				letterButtons[i].setTitle(letterBits[i], for: .normal)
+		
+		DispatchQueue.main.async {
+			[weak self] in
+			//configure buttons anf labels
+			self?.cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+			self?.answerLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+			letterBits.shuffle()
+			
+			let count = self?.letterButtons.count
+			if letterBits.count == count {
+				for i in 0 ..< count! {
+					self?.letterButtons[i].setTitle(letterBits[i], for: .normal)
+				}
 			}
 		}
+
 	}
 
 	func levelup (action: UIAlertAction) {
 //		level += 1
 		solutions.removeAll(keepingCapacity: true)
-		
 		loadLevel()
-		
 		for b in letterButtons {
 			b.isHidden = false
 		}
