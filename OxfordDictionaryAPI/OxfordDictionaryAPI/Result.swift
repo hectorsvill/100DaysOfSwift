92c610b1fe800b180a548e7c357c1aacefd00381
hvillasa <hvillasano@go.pasadena.edu>
2019-03-17 10:51:57 -0700

    project 10

---
+++ OxfordDictionaryAPI/OxfordDictionaryAPI/Result.swift
@@ -0,0 +1,9 @@
+//
+//  Result.swift
+//  OxfordDictionaryAPI
+//
+//  Created by Hector Steven on 3/15/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
---
+++ OxfordDictionaryAPI/OxfordDictionaryAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OxfordDictionaryAPI
+//
+//  Created by Hector Steven on 3/15/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
