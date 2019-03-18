0386f941b03b65e6f453c3c28d8a775acbddaa93
hvillasa <hvillasano@go.pasadena.edu>
2019-03-18 10:47:32 -0700

    mv

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	var persons = [Person]()
 	
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -29,36 +28,37 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 	}
 	
 	//collectionView() ////////////////////////////////////////////////////////
+
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return persons.count
 	}
-	
+
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
 			fatalError("unable to dequeue Reusable Cell")
 		}
-		
+
 		let person = persons[indexPath.item]
-		
+
 		cell.name.text = person.name
-		
+
 		let path = getDocumentsDirectory().appendingPathComponent(person.image)
 		cell.imageView.image = UIImage(contentsOfFile: path.path)
-		
+
 		cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
 		cell.imageView.layer.borderWidth = 2
 		cell.imageView.layer.cornerRadius = 3
 		cell.layer.cornerRadius = 3
-		
+
 		return cell
 	}
-	
+
 	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-		
+
 		let person = persons[indexPath.item]
-		
+
 		let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
-		
+
 		ac.addTextField()
 		ac.addAction(UIAlertAction(title: "Canel", style: .cancel))
 		
@@ -81,6 +81,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 	}
 	
 	//func() //////////////////////////////////////////////////////////////////
+
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 		guard let image = info[.editedImage] as? UIImage else { return }
 
@@ -96,7 +97,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 		collectionView.reloadData()
 		dismiss(animated: true)
 	}
-	
+
 	func getDocumentsDirectory() -> URL {
 		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 		return paths[0]
