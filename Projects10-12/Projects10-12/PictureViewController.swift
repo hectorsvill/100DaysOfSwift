77b6b53dda65c6acd70e2c1f2ff00d8fcaec48c1
hectorsvill <hvillasano@go.pasadena.edu>
2019-03-23 19:20:32 -0700

    milestone Project

--- Projects10-12/Projects10-12/PictureViewController.swift
+++ Projects10-12/Projects10-12/PictureViewController.swift
@@ -12,13 +12,72 @@ class PictureViewController: UIViewController {
 	var picture: PicturesCaption?
 	var path: String?
 	
+	@IBOutlet var pictureCaptionLabel: UILabel!
+	@IBOutlet var pictureTitleLabel: UILabel!
 	@IBOutlet var selectedImageView: UIImageView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTitleOrLable))
 		if let image = path {
 			self.selectedImageView?.image = UIImage(contentsOfFile: image)
 		}
+		if let imageName = picture?.imageName {
+			pictureTitleLabel.text = imageName
+			
+		}
+		if let imageCaption = picture?.imageCaption {
+			pictureCaptionLabel.text = imageCaption
+		}
+	}
+	
+	@objc func editTitleOrLable() {
+		let ac = UIAlertController(title: "Edit Picture Information", message: nil, preferredStyle: .actionSheet)
+		ac.addAction(UIAlertAction(title: "Edit Title", style: .default) {
+			[weak self] _ in
+			self?.editTitle()
+		})
+		ac.addAction(UIAlertAction(title: "Edit Caption", style: .default) {
+			[weak self] _ in
+			self?.editCaption()
+		})
+		ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		present(ac, animated: true)
+	}
+	
+	func editTitle () {
+		alertTextField(tc: "Title")
+	}
+	
+	func editCaption() {
+		alertTextField(tc: "Caption")
+	}
+	
+	func alertTextField(tc: String) {
+		let ac = UIAlertController(title: "Edit \(tc)", message: nil, preferredStyle: .alert)
+		ac.addTextField()
+		ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: {
+			[weak self] _ in
+			guard let str = ac.textFields?[0].text else { return }
+			
+			self?.editPictureClass(to: str, label: tc)
+		
+		}))
+		present(ac, animated: true)
+	}
+	
+	func editPictureClass (to str: String, label: String) {
+		
+		if label == "Title" {
+			pictureTitleLabel.text = str
+			picture?.imageName = str
+
+		} else if label == "Caption" {
+			pictureCaptionLabel.text = str
+			picture?.imageCaption = str
+
+		}
+//			ViewController.save()
 	}
 }
