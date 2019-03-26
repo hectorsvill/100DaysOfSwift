b8193a7adf04bd7a4432d1f8f735483a973da9a9
hectorsvill <hvillasano@go.pasadena.edu>
2019-03-25 17:05:02 -0700

    Project13 fix

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,21 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-	@IBOutlet var imageView: UIImageView!
-	@IBOutlet var intensity: UISlider!
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	
+	
+	@IBOutlet var intensitySlider: UISlider!
+	@IBOutlet var imageView: UIView!
+	var currentImage: UIImage!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		title = "InstaFiter"
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
 	}
 
-	@IBAction func changeFilter(_ sender: Any) {
+	// import Picture ////////////////////////////////////////////////////////
+	
+	@objc func importPicture() {
+		let picker = UIImagePickerController()
+		picker.allowsEditing = true
+		picker.delegate = self
+		present(picker, animated: true)
 	}
 	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let image = info[.editedImage] as? UIImage else { return }
+		dismiss(animated: true)
+		currentImage = image
+	}
+	
+	@IBAction func changeFilter(_ sender: Any) {
+	}
+
 	@IBAction func save(_ sender: Any) {
 	}
-	@IBAction func intensityChanged(_ sender: Any) {
+
+	@IBAction func IntensityChanged(_ sender: Any) {
 	}
 }
 
