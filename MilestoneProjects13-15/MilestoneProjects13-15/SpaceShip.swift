0decf9be1c9d7ddde58df70ef2f402ef42424f0f
hectorsvill <hvillasano@go.pasadena.edu>
2019-04-10 16:16:40 -0700

    extension

---
+++ MilestoneProjects13-15/MilestoneProjects13-15/SpaceShip.swift
@@ -0,0 +1,14 @@
+//
+//  SpaceShip.swift
+//  MilestoneProjects13-15
+//
+//  Created by Hector Steven on 4/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import SpriteKit
+
+
+class SpaceShip {
+	
+}
