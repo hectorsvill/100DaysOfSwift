dd0efe2caece992c4de8c72d659e665fee1bc614
hectorsvill <hvillasano@go.pasadena.edu>
2019-04-16 14:10:44 -0700

    hnotes - ios notes app

---
+++ helloworld/helloworld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  helloworld
+//
+//  Created by Hector Steven on 4/14/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Hector Steven on 4/14/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
