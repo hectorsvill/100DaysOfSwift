6c81888cf04c3e5aaa5cff9b490dc34218d59bf8
hectorsvill <hvillasano@go.pasadena.edu>
2019-04-16 20:28:37 -0700

    restart

--- hNotes/hNotes/ViewController.swift
+++ hNotes/hNotes/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		title = "Folders"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(edit))
 	}
 
-
+	
 }
 
+
+extension ViewController {
+	@objc func edit () {
+		print("print tableview")
+	}
+}
