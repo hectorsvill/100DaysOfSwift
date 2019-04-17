291ab0a0a1596494c21847569c9302d73af958ea
hectorsvill <hvillasano@go.pasadena.edu>
2019-04-16 22:07:27 -0700

    tableview

--- hNotes/hNotes/ViewController.swift
+++ hNotes/hNotes/ViewController.swift
@@ -10,11 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 	
-	
-
-	@IBOutlet var newFolderButton: UIButton!
-	@IBOutlet var FoldersTableView: FolderTableView!
-	
+	@IBOutlet var FolderTableView: UITableView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -25,13 +21,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(edit))
 		navigationItem.rightBarButtonItem?.tintColor = .green
 
-		FoldersTableView.dataSource = self
-		FoldersTableView.delegate = self
+		FolderTableView.dataSource = self
+		FolderTableView.delegate = self
 	}
 	
-	
 	@IBAction func NewFolderButton(_ sender: Any) {
-		CreateNewFolder()
+		
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,27 +34,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = FoldersTableView.dequeueReusableCell(withIdentifier: "FolderCellId", for: indexPath)
+		let cell = FolderTableView.dequeueReusableCell(withIdentifier: "FolderCellId", for: indexPath)
 		
 		cell.textLabel?.text = "\(indexPath.row)"
 		return cell
 	}
-	
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		print("\(indexPath.row)")
-	}
-	
-	
 }
 
 extension ViewController {
-	
-	func CreateNewFolder() {
+	@objc func edit () {
 		let ac = UIAlertController(title: "New Folder", message: "Enter a name for this folder.", preferredStyle: .alert)
 		ac.addTextField()
 		ac.addAction(UIAlertAction(title: "Save", style: .default, handler: {
 			[weak self ] _ in
-			print("save")
+			print("get str")
 		}))
 		
 		
@@ -67,9 +55,5 @@ extension ViewController {
 		
 		
 		present(ac, animated: true)
-	}
-	
-	@objc func edit () {
-		
 	}
 }
