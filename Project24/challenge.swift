9154147ff68b6c7af3d8d1737b11ef6a92cf87ba
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-27 23:48:49 -0700

    challenge

---
+++ Project24/challenge.swift
@@ -0,0 +1,36 @@
+#!/usr/bin/swift
+import Foundation
+
+
+/*
+	Create a String extension that adds a withPrefix() method.
+	If the string already contains the prefix it should return
+	itself; if it doesn’t contain the prefix, it should return
+	itself with the prefix added. For
+	example: "pet".withPrefix("car") should return “carpet”.A
+*/
+
+let str = """
+m
+e
+"""
+
+extension String {
+	func withPrefix() {
+		print("yes")
+	}
+}
+str.withPrefix()
+
+
+
+
+
+
+
+
+
+
+
+
+
