9de971adf0ef85f67c6491e1cef6aac0d13d6659
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-27 19:26:00 -0700

    Strings are not arrays

--- Project24/Project24.playground/Contents.swift
+++ Project24/Project24.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
+//indexing thorugh a string
+
+let name = "Taylor"
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+	subscript(i: Int) -> String {
+		return String(self[index(startIndex, offsetBy: i)])
+	}
+}
+
+let l = name[2]
