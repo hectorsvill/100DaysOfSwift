9d535bf2f0ddc755427272fddabc874f1244bc8b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-28 00:08:15 -0700

    Exntensions

--- Project24/challenge.swift
+++ Project24/challenge.swift
@@ -6,7 +6,7 @@ import Foundation
 	Create a String extension that adds a withPrefix() method.
 	If the string already contains the prefix it should return
 	itself; if it doesn’t contain the prefix, it should return
-	itself with the prefix added. For/Users/hectorsteven/dev/100DaysOfSwift/Project24/Project24.swift
+	itself with the prefix added. For
 	example: "pet".withPrefix("car") should return “carpet”.A
 */
 
@@ -23,13 +23,26 @@ extension String {
 	}
 }
 
-let test = str.withPrefix("I love ")
-print(test)
+//let test = str.withPrefix("I love ")
+//print(test)
 
 
+/*
+Create a String extension that adds an isNumeric property
+that returns true if the string holds any sort of number.
+Tip: creating a Double from a String is a failable initializer.
+
+*/
+
+extension String {
+	let isNumeric: Bool = {
+		return false
+	}
+}
 
 
-
+let num = "This number 42 is no luck!"
+print(num.isNumeric)
 
 
 
