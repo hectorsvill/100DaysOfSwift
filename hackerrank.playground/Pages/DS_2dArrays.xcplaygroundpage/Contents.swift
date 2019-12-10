826998562cef55dfdd3ef3263f138defc8086806
hectorsvill <hectorstevenvillasano@gmail.com>
2019-12-09 21:30:11 -0800

    hourglass sum

--- hackerrank.playground/Pages/DS_2dArrays.xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/DS_2dArrays.xcplaygroundpage/Contents.swift
@@ -18,29 +18,4 @@ func hourglassSum(arr: [[Int]]) -> Int {
     print(values)
     return values.max()!
 }
-
-let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
-FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
-let fileHandle = FileHandle(forWritingAtPath: stdout)!
-
-let arr: [[Int]] = AnyIterator{ readLine() }.prefix(6).map {
-    let arrRow: [Int] = $0.split(separator: " ").map {
-        if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
-            return arrItem
-        } else { fatalError("Bad input") }
-    }
-
-    guard arrRow.count == 6 else { fatalError("Bad input") }
-
-    return arrRow
-}
-
-guard arr.count == 6 else { fatalError("Bad input") }
-
-let result = hourglassSum(arr: arr)
-
-fileHandle.write(String(result).data(using: .utf8)!)
-fileHandle.write("\n".data(using: .utf8)!)
-
-
-//: [Next](@next)
+    
