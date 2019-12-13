a706e9519ea40e39824718a96146e2d324ee5d9f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-12-13 09:30:09 -0800

    grading problem

--- hackerrank.playground/Pages/Three Number Sum.xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/Three Number Sum.xcplaygroundpage/Contents.swift
@@ -1,7 +1,56 @@
 //: [Previous](@previous)
 
 import Foundation
+// https://www.hackerrank.com/challenges/grading/problem
 
-var str = "Hello, playground"
+func getNextFithMultiple(_ num: Int) -> Int{
+    var f = num
+    
+    while f % 5 != 0 {
+        f += 1
+    }
+    
+    return f
+}
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    
+    var new_grade = [Int]()
+    
+    for grade in grades {
+        if grade < 38 {
+            new_grade.append(grade)
+        } else {
+            if grade % 5 == 0 {
+                new_grade.append(grade)
+            } else {
+                let next = getNextFithMultiple(grade)
+                let value = next - grade < 3 ? next : grade
+                new_grade.append(value)
+                    
+            }
+        }
+    }
+    
+    return new_grade
+}
+
+
+
+
+
+
+
+
+
+let x = [73, 67 ,38 , 33]
+
+print(gradingStudents(grades: x))
+
+
+
+
+
+
 
 //: [Next](@next)
