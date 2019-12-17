e7f323874b0f31ebf1b062aa1a49e473387a3660
hectorsvill <hectorstevenvillasano@gmail.com>
2019-12-16 20:13:04 -0800

    get quote links

--- webscrape/Sources/webscrape/main.swift
+++ webscrape/Sources/webscrape/main.swift
@@ -1,10 +1,10 @@
 import Foundation
 import SwiftSoup
 
-var dict: [String: [[String: String]]] = [:]
+var QuotesDictionary: [String: [[String: Any]]] = [:]
 
-func scrapeHistoryQuotes() -> [[String: String]] {
-    var dict: [[String: String]] = []
+func scrapeHistoryQuotes() -> [[String: Any]] {
+    var dict: [[String: Any]] = []
     let myStrKey = "https://wisdomquotes.com/history-quotes/"
     let url = URL(string: "https://wisdomquotes.com/history-quotes/")!
 
@@ -13,17 +13,22 @@ func scrapeHistoryQuotes() -> [[String: String]] {
         let quoteElements = try! doc.select("blockquote")
         
         for i in 0..<quoteElements.count {
-            var d: [String: String] = [:]
             
-            if let str = try? quoteElements[i].text() {
+            do {
                 
+                let str = try quoteElements[i].text()
                 let quoteAndAuth = str.split(separator: ".")
+                
                 let body = String(quoteAndAuth[0])
                 
-                d["body"] = body
-                d["author"] = quoteAndAuth.count >= 2 ? String(quoteAndAuth[1]) : ""
+                dict.append([
+                    "body": body,
+                    "author": quoteAndAuth.count >= 2 ? String(quoteAndAuth[1]) : "",
+                    "tags": ["history"]
+                ])
                 
-                dict.append(d)
+            } catch {
+                NSLog("Error: \(error)")
             }
         }
     } else {
@@ -37,6 +42,42 @@ func scrapeHistoryQuotes() -> [[String: String]] {
     return dict
 }
 
-let historyQuotes = scrapeHistoryQuotes()
-dict["history"] = historyQuotes
-print(dict)
+//let historyQuotes = scrapeHistoryQuotes()
+//QuotesDictionary["history"] = historyQuotes
+//print(QuotesDictionary)
+
+
+func getAllQuoteLinks() -> [String] {
+    var links = [String]()
+    let url = URL(string: "https://wisdomquotes.com/")!
+    let myStrKey = "wisdomquotesLinks"
+    
+    if let mainPage = UserDefaults.standard.string(forKey: myStrKey) {
+        do{
+            let doc = try SwiftSoup.parse(mainPage)
+            let quoteLinks = try doc.select("a")
+//            print(quoteLinks[10])
+//            let x = try quoteLinks[10].attr("href", true)
+//            print(x)
+            
+            for i in 10..<quoteLinks.count - 9 {
+                
+                var str = try quoteLinks[i].text()
+                str += "-quotes"
+                links.append("https://wisdomquotes.com/" + str.lowercased())
+            }
+            
+        } catch {
+            NSLog("Error: \(error)")
+        }
+    } else {
+        let data = try! Data(contentsOf: url)
+        let str = String(data: data, encoding: .utf8)
+        UserDefaults.standard.set(str, forKey: myStrKey)
+    }
+    
+    return links
+}
+
+
+getAllQuoteLinks().map { print($0) }
