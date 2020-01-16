d8f2d058842af889eae13fd9dbb3c223e811e552
hectorsvill <hectorstevenvillasano@gmail.com>
2020-01-16 13:02:23 -0800

    balancedStrings clean up

--- hackerrank.playground/Pages/split-a-string-in-balanced-strings.xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/split-a-string-in-balanced-strings.xcplaygroundpage/Contents.swift
@@ -3,30 +3,19 @@
 // https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
 class Solution {
-    
-//    init () {
-//        print(balancedStringSplit("RLRRLLRLRL"))
-//    }
-    
     func balancedStringSplit(_ s: String) -> Int {
         var splitCount = 0
         var rCount = 0
         var lCount = 0
         
         for c in s {
-            if c == "R" {
-                rCount += 1
-            } else if c == "L" {
-                lCount += 1
-            }
+            c == "R" ? (rCount += 1) : (lCount += 1)
             
             if rCount == lCount {
                 splitCount += 1
                 rCount = 0
                 lCount = 0
             }
-            
-            print(c)
         }
         return splitCount
     }
