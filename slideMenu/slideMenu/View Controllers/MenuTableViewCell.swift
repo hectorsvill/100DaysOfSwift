7b420b77efe8d5185d44c1a6a73df199382963fb
hectorsvill <hectorstevenvillasano@gmail.com>
2020-01-18 17:26:04 -0800

    basic side menu

--- slideMenu/slideMenu/View Controllers/MenuTableViewCell.swift
+++ slideMenu/slideMenu/View Controllers/MenuTableViewCell.swift
@@ -20,20 +20,39 @@ class MenuTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Sample here"
+        
+        return label
+    }()
+    
     private func setupView() {
-        iconImageview.image = UIImage(systemName: "")
+        iconImageview.image = UIImage(systemName: "wifi")
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .gray
-        iconImageview.translatesAutoresizingMaskIntoConstraints = true
+        
+        iconImageview.translatesAutoresizingMaskIntoConstraints = false
+        iconImageview.image = UIImage(systemName: "wifi")
         addSubview(iconImageview)
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(descriptionLabel)
+        
         NSLayoutConstraint.activate([
             iconImageview.centerYAnchor.constraint(equalTo: centerYAnchor),
             iconImageview.leftAnchor.constraint(equalTo: leftAnchor),
             iconImageview.heightAnchor.constraint(equalToConstant: 24),
             iconImageview.widthAnchor.constraint(equalToConstant: 24),
+            
+            descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            descriptionLabel.leftAnchor.constraint(equalTo: iconImageview.rightAnchor, constant: 16)
+
         ])
     }
     
