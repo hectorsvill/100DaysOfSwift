277ad53abe0c30790e9622e41264f358d465bfbe
hectorsvill <hectorstevenvillasano@gmail.com>
2020-02-18 19:20:56 -0800

    DropDownMenuView

--- InsetItemGrid/InsetItemGrid/View Controllers/DropDownMenuViewController.swift
+++ InsetItemGrid/InsetItemGrid/View Controllers/DropDownMenuViewController.swift
@@ -9,6 +9,35 @@
 import UIKit
 
 class DropDownMenuViewController: UIViewController {
+
+
+    var dropDownButton = DropDownButton()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        createButton()
+    }
+
+    private func createButton() {
+        dropDownButton = DropDownButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        dropDownButton.translatesAutoresizingMaskIntoConstraints = false
+        dropDownButton.setTitle("Words", for: .normal)
+        view.addSubview(dropDownButton)
+
+        let inset: CGFloat = 8
+        NSLayoutConstraint.activate([
+            dropDownButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: inset),
+            dropDownButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: inset),
+            dropDownButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -inset),
+            dropDownButton.widthAnchor.constraint(equalToConstant: 100),
+            dropDownButton.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+}
+
+class DropDownMenuView: UIView, UITableViewDelegate {
     enum Section {
         case main
     }
@@ -16,27 +45,29 @@ class DropDownMenuViewController: UIViewController {
     var tableView: UITableView! = nil
     var dataSource: UITableViewDiffableDataSource<Section, Int>! = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
         createTableView()
     }
-}
 
-extension DropDownMenuViewController: UITableViewDelegate {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 
     private func createTableView() {
         tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        view.addSubview(tableView)
+        addSubview(tableView)
 
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor),
         ])
 
         dataSource = UITableViewDiffableDataSource<Section, Int>(tableView: tableView, cellProvider: { tableView, indexPath, i -> UITableViewCell? in
@@ -54,6 +85,24 @@ extension DropDownMenuViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
+    }
+
+
+}
+
+
+// MARK: DROPDOWNBUTTON
+
+class DropDownButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundColor = .systemGray2
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 
