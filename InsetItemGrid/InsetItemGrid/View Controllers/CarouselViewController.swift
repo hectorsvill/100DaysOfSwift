dec9339b7dac37a9e2c96e96ea3d7352c104bb7b
hectorsvill <hectorstevenvillasano@gmail.com>
2020-02-23 08:17:51 -0800

    UICollectionViewDiffableDataSource

--- InsetItemGrid/InsetItemGrid/View Controllers/CarouselViewController.swift
+++ InsetItemGrid/InsetItemGrid/View Controllers/CarouselViewController.swift
@@ -10,10 +10,48 @@ import UIKit
 
 class CarouselViewController: UIViewController {
 
+    enum Section {
+        case main
+    }
+
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        createCollectionView()
+    }
+
+    private func createCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+        ])
+
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView, cellProvider: { collectionView, indexPath, i -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+            cell.backgroundColor = .gray
+            return cell
+        })
+
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Int>()
+
+        snapShot.appendSections([.main])
+        snapShot.appendItems(Array(0...10))
+        dataSource.apply(snapShot, animatingDifferences: true, completion: nil)
 
     }
 }
