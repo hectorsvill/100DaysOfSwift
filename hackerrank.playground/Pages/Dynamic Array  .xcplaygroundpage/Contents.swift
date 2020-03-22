8a9014c2b6b8e8196a74a423c11c1e42fbeb8a7e
hectorsvill <hectorstevenvillasano@gmail.com>
2020-03-22 11:12:50 -0700

    dynamicArray with dictionary

--- hackerrank.playground/Pages/Dynamic Array  .xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/Dynamic Array  .xcplaygroundpage/Contents.swift
@@ -37,25 +37,27 @@ let arr = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]
 
 func dynamicArray(n: Int, queries: [[Int]]) -> [Int] {
     var lastAnswer = 0
-    var seqList: [[Int]] = []
+    var seqList: [Int: [Int]] = [:]
     var result = [Int]()
 
-    for _ in 0..<n { seqList.append([]) }
+//    for _ in 0..<n { seqList.append([]) }
 
     queries.forEach {
         let operation = $0[0]
         let seqListIndex = ($0[1] ^ lastAnswer) % n
 
         if operation == 1 {
-            seqList[seqListIndex].append($0[2])
+            seqList[seqListIndex, default: []].append($0[2])
+//            seqList[seqListIndex].append($0[2])
         } else if operation == 2 {
-            let size = seqList[seqListIndex].count
-            let item = seqList[seqListIndex][$0[2] % size]
+            let size = seqList[seqListIndex]!.count
+            let item = seqList[seqListIndex]![$0[2] % size]
             lastAnswer = item
             print(lastAnswer)
             result.append(lastAnswer)
         }
     }
+//    print(seqList)
     return result
 }
 
