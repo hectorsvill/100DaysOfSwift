978218e786ec8b32a41bd329d542103693a3ae53
hectorsvill <hectorstevenvillasano@gmail.com>
2020-04-14 19:07:37 -0700

    jumping on clouds

--- hackerrank.playground/Pages/Kangaroo.xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/Kangaroo.xcplaygroundpage/Contents.swift
@@ -10,20 +10,30 @@ func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
     var position_x1 = x1
     var position_x2 = x2
 
-    while (position_x1 <= 1000 || position_x2 <= 1000) {
-
-        if (position_x1 == position_x2) {
-            return "YES"
-        }
+    while (position_x1 <= Int.max || position_x2 <= Int.max) {
 
         position_x1 += v1
         position_x2 += v2
 
-    }
-    
-    if (position_x1 == position_x2) {
-            return "YES"
+        if (position_x1 == position_x2) {
+            break
+        }
     }
 
-    return "NO"
+//    print("x1: \(position_x1 )\nx2: \(position_x2)")
+
+    return (position_x1 == position_x2) ? "YES" : "NO"
 }
+
+//4523 8092 9419 8076 , yess
+
+let x1 = 4523
+let v1 = 8092
+let x2 = 9419
+let v2 = 8076
+
+let resutl = kangaroo(x1: x1, v1: v1, x2: x2, v2: v2)
+
+print(resutl)
+
+
