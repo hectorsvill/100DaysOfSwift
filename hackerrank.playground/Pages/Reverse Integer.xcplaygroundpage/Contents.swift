8a91ae45310ff69ccef2df88ff48cd7b939a3a25
hectorsvill <hectorstevenvillasano@gmail.com>
2020-08-11 11:39:36 -0700

    refactor reverse

--- hackerrank.playground/Pages/Reverse Integer.xcplaygroundpage/Contents.swift
+++ hackerrank.playground/Pages/Reverse Integer.xcplaygroundpage/Contents.swift
@@ -2,38 +2,27 @@
 
 import Foundation
 import XCTest
-
-
 /*
  https://leetcode.com/problems/reverse-integer/
  
  Given a 32-bit signed integer, reverse digits of an integer.
  Hint: 32-bit signed integer range: [−2^31,  2^31 − 1].
- 
- Example 1
- ----------
- input: 123
- output: 123
- 
- Example 2
- ----------
- input: -123
- output: -321
- 
- Example 3
- ----------
- Input: 120
- Output: 21
+
  */
 
-let max = pow(2, 31) - 1
-let min = -pow(2, 31)
-
+extension Int {
+    public static var min32Bit: Int {
+        NSDecimalNumber(decimal: (pow(2, 31) * -1)).intValue
+    }
+    
+    public static var max32Bit: Int {
+        NSDecimalNumber(decimal: (pow(2, 31) - 1)).intValue
+    }
+}
 
 func reverse(_ x: Int) -> Int {
     var number = x
     var reverseNumber = 0
-    
     
     if x < 0 {
         number *= -1
@@ -45,8 +34,7 @@ func reverse(_ x: Int) -> Int {
         number /= 10
     }
     
-
-    if reverseNumber < -2147483648 || reverseNumber > 2147483647 {
+    if reverseNumber < Int.min32Bit || reverseNumber > Int.max32Bit {
         return 0
     } else if x <  0 {
         reverseNumber *= -1
@@ -59,7 +47,6 @@ XCTAssertEqual(reverse(-56789), -98765)
 XCTAssertEqual(reverse(123), 321)
 XCTAssertEqual(reverse(12300), 321)
 XCTAssertEqual(reverse(1534236469), 0)
-
 
 //: [Next](@next)
 
